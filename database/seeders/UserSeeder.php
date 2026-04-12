a54c0d3a7a1d4921802d51f718b1bc28986505cc
hidekiti42 <youngjoon029@gmail.com>
2026-04-12 20:49:52 +0900

    UserSeeder追加

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,10 +21,10 @@ class UserSeeder extends Seeder
         $user->name = "英俊";
         $user->email = 'youngjoon029@gmail.com';
         $user->email_verified_at = Carbon::now();
-        $user->password = Hash::make('3df36eea78f22e');
-        $user->postal_code = "0000000";
-        $user->address = "東京都";
-        $user->phone = "000-0000-0000";
+        $user->password = Hash::make(3df36eea78f22e);
+        $user->postal_code = "577-0818";
+        $user->address = "大阪府東大阪市小若江2-1-15";
+        $user->phone = "070-8511-1077";
         $user->save();
     }
 }
