ac73f8753d6c0c2467d087ca9b3dd00c409833cd
hidekiti42 <youngjoon029@gmail.com>
2026-04-12 14:25:08 +0900

    Add fillable to Review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,6 +9,14 @@ class Review extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'score',
+        'title',
+        'content',
+    ];
+
     public function product()
     {
         return $this->belongsTo(Product::class);
