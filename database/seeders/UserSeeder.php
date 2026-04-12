f85f9d6e771b38dc97bf3b462cdf9bcf27e6f740
hidekiti42 <youngjoon029@gmail.com>
2026-04-12 20:53:23 +0900

    Fix syntax error in UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,7 +21,7 @@ class UserSeeder extends Seeder
         $user->name = "英俊";
         $user->email = 'youngjoon029@gmail.com';
         $user->email_verified_at = Carbon::now();
-        $user->password = Hash::make(3df36eea78f22e);
+        $user->password = Hash::make('3df36eea78f22e');
         $user->postal_code = "577-0818";
         $user->address = "大阪府東大阪市小若江2-1-15";
         $user->phone = "070-8511-1077";
